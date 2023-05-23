d33e55bb2450dcfbcf7896632c46b76f07afe826
duyphuong220502 <phuongduy220502@gmail.com>
2023-05-23 22:37:45 +0700

    layout-web

--- datn_anime_store/src/main/java/com/anime/controller/admin/CategoryParentController.java
+++ datn_anime_store/src/main/java/com/anime/controller/admin/CategoryParentController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminHomeController {
-	@GetMapping("/admin/index")
-	public String doShowIndex() {
-		return "admin/index";
+public class CategoryParentController {
+	@GetMapping("/admin/category-parent")
+	public String doShowView() {
+		return "admin/category-parent";
 	}
 }
